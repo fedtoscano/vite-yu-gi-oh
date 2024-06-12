bbae9d1f8b639184b9315c857026aafb87e8da7e
fedtoscano <federicotoscano611@gmail.com>
2024-06-12 15:56:23 +0200

    MainHeader implementato

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -7,8 +7,26 @@ return {
 </script>
 
 <template>
-    <h1>ciao da header app</h1>
+    <div class="container">
+        <div class="img-container">
+            <img src="../assets/yugioh-logo.png" alt="YuGiOh main logo">
+        </div>
+        <h2>Yu-Gi-Oh API</h2>
+    </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.container{
+    display: flex;
+    align-items: center;
+
+    .img-container{
+        width: 100px;
+        margin-right: 2em;
+
+        img{
+            width: 100%;
+        }
+    }
+}
 </style>
--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -12,9 +12,18 @@ return {
 </script>
 
 <template>
-    <h1>ciao dal main App</h1>
-    <CardList/>
+    <div class="container">
+        <h1>ciao dal main App</h1>
+        <CardList/>
+
+    </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use "../style/partials/variables" as *;
+
+.container{
+    background-color: $orange;
+}
+
 </style>
