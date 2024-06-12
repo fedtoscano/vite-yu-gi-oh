4eaedd7fdbc92c451125cb0130cd509392e0ca66
fedtoscano <federicotoscano611@gmail.com>
2024-06-12 16:24:42 +0200

    chiamata API implementata con successo

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,6 +1,6 @@
 <script>
 import CardList from "./cardComponents/CardList.vue";
-
+import axios from  "axios"
 export default {
 components:{
     CardList,
@@ -12,9 +12,25 @@ return {
 },
 methods: {
     getCards: function(){
-        
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=40&offset=0')
+            .then(function (response) {
+                // handle success
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+    },
+
+},
+created(){
+        this.getCards()
+    },
     }
-}}
 </script>
 
 <template>
